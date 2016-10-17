435c0b136312587d9de98626f28d986593f42eb3
AnitaLviv <annet_v@ukr.net>
2016-10-17 20:29:52 +0300

    JS 11

--- js/script.js
+++ js/script.js
@@ -1,21 +1,37 @@
-var a = prompt( 'Введите  число', '' );
-var b = prompt( 'Введите  степень', '' );
+(function($) {
 
-function pow( a, b ) {
-  var result = 1;
-  
-  if (b < 0) {
-    return 1 / pow(a, -b);
-  }
-  
-  for( var i = 0; i < b; i++ ) {
-    result *= a;
-  }  
-  
-  return result;  
- }
+  var sliderUl = $("div.slider").css("overflow","hidden").children("ul"),
+      imgs = sliderUl.find("img"),
+      imgWidth= imgs[0].width,
+      imgsLen = imgs.length,
+      current = 1,
+      totalImgsWidth = imgsLen * imgWidth;
 
 
-var bigResult = pow( a, b );
-console.log( bigResult );
-
+    $("#slider-nav").show().find("button").on("click", function() {
+     var direction = $(this).data("dir");
+        loc = imgWidth;
+        
+        (direction === "next") ? ++current : --current;
+        if(current === 0) {
+            current =imgsLen;
+            loc = totalImgWidth -imgWidth;
+            dirrection = "next";
+        }else if ( current -1 === imgsLen ) {
+            current = 1;
+            loc=0;
+        }
+	transition( sliderUl, loc , direction );
+    });
+    function transition( container, loc, direction ) {
+        var unit;
+        
+        if(direction && loc !== 0) {
+            unit = (direction === "next") ? "-=" : "+=";
+        }
+        container.animate({
+            "margin-left": unit ? (unit + loc) : loc
+        });
+        
+    }
+})(jQuery);
